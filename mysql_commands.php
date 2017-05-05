c0d2362f981df77482f8c56bcb2c5ffa29c7a59e
Filip Hansen <hanfil@outlook.com>
2017-05-05 18:50:20 +0200

    changes

--- mysql_commands.php
+++ mysql_commands.php
@@ -1,31 +1,35 @@
 <?php
 //Connection settings | MySql
 define('DB_HOST', 'localhost');
+define('DB_NAME', 'BuSy')
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 //Connect to database
-function mysql_conn($dbname, $force){
-    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,$dbname);
+function mysql_conn($force){
+    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     if ($mysqli->connect_errno) {
         printf("Connect failed: %s\n", $mysqli->connect_error);
         exit();
         if($force == 'yes'){
-            $query="CREATE DATABASE".$dbname;
-            mysql_ask($query,'update');
+            $query="CREATE DATABASE".DB_NAME;
+            mysql_ask('update', $query);
         }
     }
 }
 
 //Query the database for information
-function mysql_ask($query, $task){
+function mysql_ask($task, $query){
     //Different task are: 'resultnumber', 'fetch', 'update'
 
     if($task == 'resultnumber'){
+        mysql_conn('');
         $run = $mysqli->query($query);
         return $run->num_rows;
+        $mysqli->close();
     }
     elseif($task == 'fetch'){
+
         $run = $mysqli->query($query);
         return $run->fetch_array(MYSQLI_BOTH);
         /*Example to use the return:
